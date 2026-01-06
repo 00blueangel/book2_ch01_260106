7407f12f86da0bdc2a92258c55450dd976992036
00blueangel <00blueangel@naver.com>
2026-01-06 12:18:11 +0900

    신상정보 Form

--- main.py
+++ main.py
@@ -1,17 +1,53 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from typing import Optional
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    obj = {
-        "message": "안녕하세요!",
-        "user": "bhy"   
-    }
-    return obj
-@app.get("/hello")
-def hell(name="hong"):
-    print("name is", name)
     return {
-        "name":name
+        "message": "안녕하세요!",
+        "user": "KBJ"
+    }
+
+@app.get("/hello")
+def hello(
+    name: Optional[str] = "홍길동",
+    address: Optional[str] = "",
+    phone: Optional[str] = "",
+    email: Optional[str] = "",
+    age: Optional[int] = 0,
+    gender: Optional[str] = ""
+):
+    print("[GET] 사용자 정보:")
+    print(f"성명: {name}, 주소: {address}, 전화번호: {phone}, 이메일: {email}, 나이: {age}, 성별: {gender}")
+    return {
+        "method": "GET",
+        "name": name,
+        "address": address,
+        "phone": phone,
+        "email": email,
+        "age": age,
+        "gender": gender
+    }
+
+@app.post("/hello")
+def hello_post(
+    name: str = Form(...),
+    address: str = Form(...),
+    phone: str = Form(...),
+    email: str = Form(...),
+    age: int = Form(...),
+    gender: str = Form(...)
+):
+    print("[POST] 사용자 정보:")
+    print(f"성명: {name}, 주소: {address}, 전화번호: {phone}, 이메일: {email}, 나이: {age}, 성별: {gender}")
+    return {
+        "method": "POST",
+        "name": name,
+        "address": address,
+        "phone": phone,
+        "email": email,
+        "age": age,
+        "gender": gender
     }
